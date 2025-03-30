ac09697a32ec72b4f94dd593be40bd136885f786
Alexandre Lehmann Holzhey <saleiro@gmail.com>
2025-03-30 16:47:06 +0200

    Add multiple servers

--- src/main.rs
+++ src/main.rs
@@ -3,9 +3,28 @@ use rsntp::SntpClient;
 
 fn main() {
     let client = SntpClient::new();
-    let server = "time.google.com";
-    let result = client.synchronize(server).unwrap();
-    let local_time: DateTime<Local> =
-        DateTime::from(result.datetime().into_chrono_datetime().unwrap());
-    print!("Current time: {}", local_time);
+    let servers = vec![
+        "time.google.com",
+        "time-a-wwv.nist.gov",
+        "time-a-b.nist.gov",
+        "time.nist.gov",
+        "utcnist.colorado.edu",
+        "pool.ntp.org",
+    ];
+    for server in servers {
+        let local_time: String = get_local_time_from_ntp_server(&client, server);
+        println!("Current time from {}: {}", server, local_time);
+    }
+}
+
+fn get_local_time_from_ntp_server(client: &SntpClient, server: &str) -> String {
+    dbg!("Retrieving clock from {}...", server);
+    match client.synchronize(server) {
+        Ok(result) => {
+            let ldt: DateTime<Local> =
+                DateTime::from(result.datetime().into_chrono_datetime().unwrap());
+            format!("{}", ldt)
+        }
+        Err(error) => error.to_string(),
+    }
 }
