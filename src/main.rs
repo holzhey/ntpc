7ca9e03b4ee5d79fc992abee6e745c72746f0b4d
Alexandre Lehmann Holzhey <saleiro@gmail.com>
2025-03-30 16:01:09 +0200

    Add simple Google NTP clock display

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,11 @@
+use chrono::{DateTime, Local};
+use rsntp::SntpClient;
+
 fn main() {
-    println!("Hello, world!");
+    let client = SntpClient::new();
+    let server = "time.google.com";
+    let result = client.synchronize(server).unwrap();
+    let local_time: DateTime<Local> =
+        DateTime::from(result.datetime().into_chrono_datetime().unwrap());
+    print!("Current time: {}", local_time);
 }
